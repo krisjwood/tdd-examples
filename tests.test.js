8186e2878e8b569e6c9e9baefc1451603e21bb2a
krjw-00 <kriswd0@gmail.com>
2021-05-31 13:30:10 +1200

    Initial commit

---
+++ tests.test.js
@@ -0,0 +1,17 @@
+const testing = require('./index.js')
+
+test('test setup working', () => {
+    expect(true).toBeTruthy()
+  })
+
+
+test('boiler test', () => {
+    // ARRANGE
+
+    // ACT
+    const actual = 0;
+    const expected = 0;
+
+    // ASSERT
+    expect(actual).toBe(expected)
+})
